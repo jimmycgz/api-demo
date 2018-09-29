3706d3d8a5dab8fb894d69c5287f0c40565f9277
Jimmy CUI <38402619+jimmycgz@users.noreply.github.com>
2018-09-29 00:53:29 -0400

    Update index.js

--- api-2-AWS/index.js
+++ api-2-AWS/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.status(200).send('API 2 is up and running!');
+    res.status(200).send('API2-AWS is up and running!');
 });
 
 app.get('/health', function(req, res) {
     res.send('Ok');
 });
 
-app.listen(5000, function() {
-    console.log('API 2 is running...');
+app.listen(4000, function() {
+    console.log('API2-AWS is running...');
 });
